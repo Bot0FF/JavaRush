03110702d37679a3f60488f524b56c989f565b3b
Bot0FF <92510704+Bot0FF@users.noreply.github.com>
2022-03-17 11:13:25 +0300

    Create USD.java

---
+++ JavaCore/Level14/lesson08/USD.java
@@ -0,0 +1,10 @@
+public class USD extends Money{
+    public USD(double amount) {
+        super(amount);
+    }
+
+    @Override
+    public String getCurrencyName() {
+        return "USD";
+    }
+}
